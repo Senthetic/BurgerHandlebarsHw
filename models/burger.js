cd6a690f9b11ce2e88eed252c32b3baccd22df16
Andrew Collier <atc3598@icloud.com>
2020-08-08 17:53:01 -0400

    completed burger.js model

--- models/burger.js
+++ models/burger.js
@@ -13,3 +13,18 @@ $(function () {
         location.reload();
       });
     });
+    $(".devourBtn").on("click", function (event) {
+        event.preventDefault();
+        const id = $(this).data("id");
+        let devouredObj = {
+          devoured: true,
+        };
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: devouredObj,
+        }).then(function () {
+          console.log("Changed burger devoured type to true");
+          location.reload();
+        });
+      });
+    });
