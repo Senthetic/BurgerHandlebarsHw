22108f68f32fbfef084a8553f4f3c26e82bb78b1
Andrew Collier <atc3598@icloud.com>
2020-08-08 17:57:44 -0400

    completed the controller

--- controller/controller.js
+++ controller/controller.js
@@ -3,3 +3,18 @@ const express = require("express");
 const app = express();
 
 const orm = require("../config/orm.js");
+
+app.get("/", function (req, res) {
+    orm.selectAll("burgers", function (res) {
+      console.log(res);
+      res.render("index");
+    });
+  });
+  
+  app.post("/api/burger", function (req, res) {
+    console.log(req.body);
+  });
+  
+  app.put("api/burger/:id", function (req, res) {});
+  
+  module.exports = app;
